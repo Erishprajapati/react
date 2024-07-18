203fb0eac4076a66fd81a279132ccf5b8c952d35
scrumbler <irishmjn@gmail.com>
2024-07-18 21:52:39 +0545

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './components/form';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <>
+    <h1> Form Validation</h1>
+<form />
+<Footer />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/form.js
@@ -0,0 +1,17 @@
+const form = () =>{
+    return (
+        <>
+        <form>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name"></input><br/>
+            <label for="name">E-mail</label>
+            <input type="text" name="Email" id="name"></input><br/>
+            <label for="name">Phone</label>
+            <input type="Number" id="number"></input><br/>
+            <label for="name">Gender</label>
+            <input type="text" name="gender" id="name"></input><br/>
+        </form>
+        </>
+    )
+}
+export default form;
