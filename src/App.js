dd9ab8369f5342284c4ab0bb7250644081bbbedd
scrumbler <irishmjn@gmail.com>
2024-07-18 22:05:33 +0545

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './components/form';
 import Footer from './components/Footer';
+
 const App = () => {
   return (
     <>
     <h1> Form Validation</h1>
+    <button>
+      I'm a button
+    </button>
 <form />
+
 <Footer />
     </>
   )
---
+++ src/components/button.js
@@ -0,0 +1,6 @@
+function MyButton (){
+    return (
+        <button className="Click">Click me</button>
+    )
+};
+export default MyButton;
