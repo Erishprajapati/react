bf62a5737b16f4b93047d31074150e904c7d968a
scrumbler <irishmjn@gmail.com>
2024-07-22 08:10:47 +0545

    some changes

---
+++ src/components/Futsal.js
@@ -0,0 +1,32 @@
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar';
+
+const App = () => {
+    return (
+      <>
+      <Navbar />
+      <h1> Form Validation</h1>
+      <div className="registration">
+        <form>
+          <label>Name:</label><br />
+          <input type="text" name="name"></input><br />
+          <label>Email:</label><br />
+          <input type="email" name="email"></input><br />
+          <label>Number:</label><br />
+          <input type="number" name="number"></input><br />
+          <label>College:</label><br />
+          <input type="text" name="name"></input><br />
+          <label>College:</label><br />
+          <input type="text" name="name"></input><br />
+        
+        </form>
+        <Footer />
+  
+      </div>
+     <button>
+      Click me
+     </button> 
+     </>
+    )
+}
+export default App;
