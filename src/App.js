22ca63f252e9032833205274d4a81181de6d660b
Erishprajapati <149457413+Erishprajapati@users.noreply.github.com>
2024-07-25 16:02:05 +0545

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import Navbar from './component/Navbar';
-import Card from './component/Card';
-import './App.css';
-const App = () =>{
+import Navbar from './Components/Navbar';
+const App = () => {
   return(
     <>
     <Navbar />
-    <Card name ="Peace lily"/>
-    <Card name ="ZZ plant"/>
-    <Card name ="Pothos"/>
-    <Card name ="Spider plant"/>
-    <Card name ="Rubber plant"/>
-    <Card name ="Snake plant"/>
-    <Card name ="Monstera deliciosa"/>
+
+
     </>
   )
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+<a href = "#" className="navbar-brand-mx-4">HimalayanSip</a>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
